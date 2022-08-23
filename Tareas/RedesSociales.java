d9f554a29adeccbf07d7725ac67e3f52a02ab96a
Undeck <omaravendanogaribay@hotmail.com>
2022-08-22 22:05:50 -0500

    Tarea

--- Tareas/RedesSociales.java
+++ Tareas/RedesSociales.java
@@ -1,4 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package redessociales;
 
+/**
+ *
+ * @author OmarA
+ */
+public class RedesSociales {
     
-
-
+    public static void main(String [] args) {
+        CSV archivo= new CSV();
+        archivo.leerArchivo("C:\\Users\\OmarA\\Desktop\\EDD_3010\\EDD_1310\\Tareas\\presenciaredes.csv");
+    }
+}
